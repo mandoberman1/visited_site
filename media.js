30270df2a6f0bcde299bd2a98515ccbe22cb6e29
mandoberman1 <147616557+mandoberman1@users.noreply.github.com>
2023-12-24 14:36:18 +0700

    Visited-site
    Небольшие изменения

--- media.js
+++ media.js
@@ -17,7 +17,7 @@ var wavesurfer = Object.create(WaveSurfer);
 
 wavesurfer.init({
     container: "#wave",
-    waveColor: "#7D7D7D", // тут можно поменять фоновый цвет плеера
+    waveColor: "white", // тут можно поменять фоновый цвет плеера
     progressColor: "#D5A216", // тут можно поменять цвет заполнения
     height: 48, // высота плеера
     scrollParent: false
